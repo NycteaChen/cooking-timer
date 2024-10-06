b428f9288853d9fd843de578d3e10b0a59603ee8
NycteaChen <jungturn01tw@gmail.com>
2024-10-06 13:25:17 +0800

    refactor(gameContext): Use contextApi to pass levelData

--- src/components/GamingCard/index.tsx
+++ src/components/GamingCard/index.tsx
@@ -1,10 +1,13 @@
-import { useMemo } from "react";
+import { useMemo, useContext } from "react";
 import { Card } from "@/components/Card";
 import { GamingRecipe } from "@/components/GamingRecipe";
 import levelRecipes from "@/utils/levelRecipes";
 import type { levelItem } from "@/utils/levelList";
+import { GameContext } from "@/pages/Game";
 
-export const GamingCard = ({ levelData }: { levelData?: levelItem }) => {
+export const GamingCard = () => {
+  const levelData = useContext(GameContext) as levelItem;
+
   const recipe = useMemo(() => {
     const randomNum = Math.floor(Math.random() * 3);
     return levelRecipes?.[levelData?.level || "easy"]?.[randomNum];
